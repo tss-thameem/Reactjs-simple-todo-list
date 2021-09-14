f332409471a4b63bbb933530f63edef86c7e857e
Thameem Ansari <thameem@tecnovators.in>
2021-09-14 20:26:16 +0530

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import axios from 'axios';
+import Loading from './Loading';
+class App extends Component {
+  state={
+      todos:[]
+  }
+ async componentDidMount() {
+    let result= await axios.get('https://jsonplaceholder.typicode.com/todos');
+     await new Promise (x=>setTimeout(x , 3000));
+    this.setState({
+        todos: result.data
+      })  
+    console.log(result);
 
-function App() {
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {this.state.todos.length>0?(<div>{this.state.todos.map(todo=>
+      <ul className="list-group">
+  <li key={todo.id}  className="list-group-item d-flex justify-content-between align-items-center">
+    {todo.title}
+    <span className="">
+    <input type ='checkbox' checked={todo.checked}/></span>
+  </li>
+  </ul>
+  )}</div>):
+      <Loading/>
+      }  
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/Loading.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Loading = () => {
+    return ( <div className="spinner-border text-primary" role="status">
+    <span className="visually-hidden">Loading...</span>
+  </div> );
+}
+ 
+export default Loading;
